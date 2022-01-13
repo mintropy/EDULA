addc3dcf5a50280d5c12604715d5247195b9372b
HTG <xorbs016@gmail.com>
2022-01-13 16:26:30 +0900

    Feat: add urls router

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    
+    class Status(models.TextChoices):
+        STUDENT = 'ST', _('Student')
+        TEACHER = 'TE', _('Teacher')
+        SCHOOLADMIN = 'SA', _('SchoolAdmin')
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        null=True,
+        blank=True,
+    )
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='student',
+        primary_key=True,
+    )
+    student_ID = models.IntegerField()
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='teacher',
+        primary_key=True,
+    )
+    teacher_ID = models.IntegerField()
+
+
+class SchoolAdmin(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='school_admin',
+        primary_key=True,
+    )
+    school_admin_ID = models.IntegerField()
