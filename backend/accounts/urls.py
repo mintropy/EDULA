841f8c1d349aa5d5672d97738238e811752a2d4f
이영준 <jun2korea@naver.com>
2022-01-14 14:00:55 +0900

    Feat: Student Serializer
    add Student Serializer
    add student API & CBV

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('student/<int:student_pk>/', views.StudentView.as_view())
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404
+from .models import (
+    User, Student, Teacher, SchoolAdmin
+)
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import StudentSerializer
 
 # Create your views here.
+class UserView(APIView):
+    
+    def get(self, request):
+        pass
+
+
+class StudentView(APIView):
+    model = Student
+    
+    def get(self, request, student_pk):
+        student = get_object_or_404(Student, pk=student_pk)
+        # student = Student.objects.get(pk=student_pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+
+class TeacherView(APIView):
+    model = Teacher
+    
+    def get(self, request):
+        pass
+
+
+class SchoolAdminView(APIView):
+    model = SchoolAdmin
+    
+    def get(self, request):
+        pass
+
