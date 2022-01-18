696ed35761eba4552e76cd18c41b6b2823b6a2f0
tjdls111 <tjdls111@naver.com>
2022-01-18 11:44:18 +0900

    Fix warnings

--- frontend/src/components/ProfileBtn.tsx
+++ frontend/src/components/ProfileBtn.tsx
@@ -1,18 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
 import { FaSmileWink } from 'react-icons/fa';
-import { _ } from '../styles/_variables';
 import NavbarDropdown from './NavbarDropdown';
-
-const ProfileImg = styled.img`
-	border-radius: 70%;
-	margin-left: 0.5rem;
-	box-shadow: 0 10px 50px gray;
-`;
-
-interface Iprops {
-	profileURL: string;
-}
 
 function Profile() {
 	const profileURL = 'https://picsum.photos/50/50';
@@ -20,7 +7,6 @@ function Profile() {
 	if (profileURL) {
 		return (
 			<span>
-				{/* <ProfileImg src={profileURL} alt='logo' /> */}
 				<NavbarDropdown />
 			</span>
 		);
