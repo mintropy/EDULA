31ea77663320dd2deeb1c0e2fc03f543d9aa41fb
tjdls111 <tjdls111@naver.com>
2022-01-19 11:22:09 +0900

    Add alarm example content and Fix warning

--- frontend/src/components/alarm/AlarmItem.tsx
+++ frontend/src/components/alarm/AlarmItem.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 interface Type {
 	type: String;
@@ -42,20 +43,20 @@ const StyledContainer = styled.p<Type>`
 	${props =>
 		props.type === '과제' &&
 		css`
-			border-color: ${props => props.theme.pointColor};
+			border-color: ${props.theme.pointColor};
 			:before {
 				content: '과';
-				background-color: ${props => props.theme.pointColor};
+				background-color: $props.theme.pointColor};
 			}
 		`}
 
 	${props =>
 		props.type === '쪽지' &&
 		css`
-			border-color: ${props => props.theme.iconColorActive};
+			border-color: ${props.theme.iconColorActive};
 			:before {
 				content: '쪽';
-				background-color: ${props => props.theme.iconColorActive};
+				background-color: ${props.theme.iconColorActive};
 			}
 		`}
 `;
@@ -71,6 +72,7 @@ function AlarmItem() {
 			title: '수학 익힘책 19-20 페이지',
 			content: '친구들 ^^ 수학 익힘책 풀어오세요~ ',
 			author: '나담임',
+			created_at: '2021.1.9',
 			link: '',
 		},
 		{
@@ -79,6 +81,7 @@ function AlarmItem() {
 			content:
 				'안녕? 잘지내지? 다음 주에 내 생일 파티에 초대할게. 4시에 우리집으로 와',
 			author: '나친구',
+			created_at: '2021.1.15',
 			link: '',
 		},
 	]);
@@ -88,7 +91,9 @@ function AlarmItem() {
 				{alarms.map(alarm => (
 					<StyledContainer type={alarm.type} key={alarm.title}>
 						<StyledTitle>{alarm.title}</StyledTitle>
-						<StyledContent>{alarm.author}</StyledContent>
+						<StyledContent>
+							{alarm.author}, {alarm.created_at}
+						</StyledContent>
 						<StyledContent>{alarm.content}</StyledContent>
 					</StyledContainer>
 				))}
