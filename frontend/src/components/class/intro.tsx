56a45a3823d32e6ad345acb466f76114617f3e42
tjdls111 <tjdls111@naver.com>
2022-01-21 09:58:36 +0900

    Fix color prop

--- frontend/src/components/class/intro.tsx
+++ frontend/src/components/class/intro.tsx
@@ -11,8 +11,8 @@ const StyledTitle = styled.h1`
 const StyledIntro = styled.div`
 	margin: 0px;
 	padding: 0px;
-	background-color: #333;
-	background-image: url('https://37.media.tumblr.com/8b4969985e84b2aa1ac8d3449475f1af/tumblr_n3iftvUesn1snvqtdo1_1280.jpg');
+	background-color: ${props => props.theme.pointColor};
+	/* background-image: url('https://37.media.tumblr.com/8b4969985e84b2aa1ac8d3449475f1af/tumblr_n3iftvUesn1snvqtdo1_1280.jpg'); */
 	background-position: center;
 	background-repeat: no-repeat;
 	background-size: cover;
