2e8267130a4b7ede234409fffde9e92802d8225a
tjdls111 <tjdls111@naver.com>
2022-01-26 10:21:56 +0900

    Delete TopNavBar in ArticleDetail and CreateArticle component

--- frontend/src/views/CreateArticle.tsx
+++ frontend/src/views/CreateArticle.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import Form from '../components/class/ArticleForm';
-import TopNavBar from '../components/navbar/TopNavBar';
 import StyledTitle from '../components/class/StyledTitle';
 
 const StyledContainer = styled.div`
@@ -10,7 +9,6 @@ const StyledContainer = styled.div`
 function CreateArticle() {
 	return (
 		<div>
-			<TopNavBar />
 			<StyledTitle>게시물 쓰기</StyledTitle>
 			<StyledContainer>
 				<Form originTitle='' originContent='' />
