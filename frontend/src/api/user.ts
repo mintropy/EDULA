f79fbdec0c00f95149daea0bc763fc9309b21889
hjyoon <se1620236@naver.com>
2022-02-04 11:09:02 +0900

    Separate port number for dev and dist

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = `${process.env.REACT_APP_PROTOCOL}://${window.location.hostname}/api`;
+const BASE_URL = `${process.env.REACT_APP_PROTOCOL}://${window.location.hostname}:${process.env.REACT_APP_PORT}/api`;
 
 const setToken = () => {
 	const token = localStorage.getItem('access') || ``;
