2a35bf61784fe777c71b7b69dc85183c052a4ade
tjdls111 <tjdls111@naver.com>
2022-02-10 13:33:02 +0900

    Implement friend list

--- frontend/src/components/friend/FriendList.tsx
+++ frontend/src/components/friend/FriendList.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Link, useParams } from 'react-router-dom';
+import StyledTitle from '../class/StyledTitle';
+import { apiGetFriendList } from '../../api/friend';
+import StyledDiv from './StyledDiv';
+import StyledContainer from './StyledContainer';
 
-function FriendList() {
-  return (
-    <div>FriendList</div>
-  )
+interface FriendDataType {
+	id: number;
+	username: string;
+	firstname: string;
+	status: string;
 }
 
-export default FriendList
+function FriendList() {
+	const [friendList, setFriendList] = useState([] as FriendDataType[]);
+	const getFriendList = () => {
+		apiGetFriendList().then(res => {
+			setFriendList(res.data);
+			console.log(res.data);
+		});
+	};
+
+	useEffect(() => {
+		getFriendList();
+	}, []);
+
+	return (
+		<div>
+			<StyledContainer>
+				<StyledTitle>친구 목록</StyledTitle>
+				{friendList &&
+					friendList.map(friend => (
+						<StyledDiv key={friend.id}>{friend.username}</StyledDiv>
+					))}
+			</StyledContainer>
+		</div>
+	);
+}
+
+export default FriendList;
