5e425e9cd74fd25b91af14f9176b8b017a395879
hjyoon <se1620236@naver.com>
2022-02-11 14:36:52 +0900

    Change to function components

--- frontend/src/views/UserVideoComponent.js
+++ frontend/src/views/UserVideoComponent.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
 import OpenViduVideoComponent from './OvVideo';
 // import './UserVideo.css';
 
-export default class UserVideoComponent extends Component {
-	getNicknameTag() {
-		// Gets the nickName of the user
-		const { streamManager } = this.props;
-		return JSON.parse(streamManager.stream.connection.data).clientData;
-	}
+function UserVideoComponent({ streamManager }) {
+	const getNicknameTag = () =>
+		JSON.parse(streamManager.stream.connection.data).clientData;
 
-	render() {
-		const { streamManager } = this.props;
-		return (
-			<div>
-				{streamManager !== undefined ? (
-					<div className='streamcomponent'>
-						<OpenViduVideoComponent streamManager={streamManager} />
-						<div>
-							<p>{this.getNicknameTag()}</p>
-						</div>
+	return (
+		<div>
+			{streamManager !== undefined ? (
+				<div className='streamcomponent'>
+					<OpenViduVideoComponent streamManager={streamManager} />
+					<div>
+						<p>{getNicknameTag()}</p>
 					</div>
-				) : null}
-			</div>
-		);
-	}
+				</div>
+			) : null}
+		</div>
+	);
 }
+
+export default UserVideoComponent;
