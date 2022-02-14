136c3006b88650ad4c5b1b525521634df4ac4332
tjdls111 <tjdls111@naver.com>
2022-02-14 17:03:41 +0900

    implement notification pagination

--- frontend/src/views/Alarm.tsx
+++ frontend/src/views/Alarm.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { apiDeleteNotification, apiGetNotifications } from '../api/notice';
-import AlarmItem from '../components/alarm/AlarmItem';
 import StyledDeleteBtn from '../components/friend/StyledDeleteBtn';
+import routes from '../routes';
+import Pagination from '../components/class/ArticlePagination';
 
 const StyledTitle = styled.h1`
 	font-size: 2em;
@@ -10,9 +12,14 @@ const StyledTitle = styled.h1`
 	margin: 1em 1em;
 	color: ${props => props.theme.fontColor};
 `;
+const StyledLink = styled(Link)`
+	text-decoration: none;
+	color: ${props => props.theme.fontColor};
+	font-size: 1.5em;
+`;
 
 const StyleRefuseBtn = styled(StyledDeleteBtn)`
-	background: ${props => props.theme.bgColor};
+	background: ${props => props.theme.subBgColor};
 	color: ${props => props.theme.fontColor};
 	box-shadow: 0 1px 3px black;
 `;
@@ -39,40 +46,66 @@ interface NotificationDataType {
 }
 
 function Alarm() {
+	const [limit, setLimit] = useState(10);
+	const [page, setPage] = useState(1);
+	const [total, setTotal] = useState(0);
+
 	const [notifications, setNotifications] = useState(
 		[] as Array<NotificationDataType>
 	);
 
-	useEffect(() => {
-		apiGetNotifications('1', '2')
+	const getNotifications = () => {
+		apiGetNotifications(page.toString(), limit.toString())
 			.then(res => {
-				setNotifications(res.data.notifications);
+				setNotifications(res.data?.notifications);
+				setTotal(res.data?.totalCount);
 			})
 			.catch(() => {});
-	}, []);
+	};
+	useEffect(() => {
+		getNotifications();
+	}, [page, limit]);
 
 	return (
 		<>
 			<StyledTitle>새 소식</StyledTitle>
-			<StyleRefuseBtn
-				onClick={e => {
-					e.preventDefault();
 
-					try {
-						apiDeleteNotification('0');
-						window.location.reload();
-					} catch (error) {
-						// console.log(error);
-					}
-				}}
-			>
-				모두 읽음
-			</StyleRefuseBtn>
+			{notifications.length !== 0 && (
+				<>
+					<label htmlFor='limit'>
+						페이지 당 표시할 소식 수:&nbsp;
+						<select
+							value={limit}
+							onChange={({ target: { value } }) => setLimit(Number(value))}
+						>
+							<option value='5'>5</option>
+							<option value='10'>10</option>
+							<option value='12'>12</option>
+							<option value='20'>20</option>
+						</select>
+					</label>
+					<StyleRefuseBtn
+						onClick={e => {
+							e.preventDefault();
+
+							try {
+								apiDeleteNotification('0');
+								window.location.reload();
+							} catch (error) {
+								// console.log(error);
+							}
+						}}
+					>
+						모두 읽음
+					</StyleRefuseBtn>
+				</>
+			)}
+
 			{notifications &&
 				notifications
 					.filter(notification => notification.notificationType === 'FQ')
 					.map(noti => (
-						<div key={noti.id}>
+						<StyledLink key={noti.id} to={routes.friend}>
 							{noti.content === null && (
 								<p>
 									{noti.fromUser?.username}({noti.fromUser?.firstName || '이름 없음'}
@@ -105,13 +138,13 @@ function Alarm() {
 							>
 								읽음
 							</StyleRefuseBtn>
-						</div>
+						</StyledLink>
 					))}
 			{notifications &&
 				notifications
 					.filter(notification => notification.notificationType === 'HC')
 					.map(noti => (
-						<div>
+						<StyledLink to={`/lecture/${noti.lecture?.id}`}>
 							<p>
 								{noti.lecture?.name}과목의 {noti.content} 과제 선물이 도착했어요!
 							</p>
@@ -129,13 +162,13 @@ function Alarm() {
 							>
 								읽음
 							</StyleRefuseBtn>
-						</div>
+						</StyledLink>
 					))}
 			{notifications &&
 				notifications
 					.filter(notification => notification.notificationType === 'HU')
 					.map(noti => (
-						<div>
+						<StyledLink to={`/lecture/${noti.lecture?.id}`}>
 							<p>
 								{noti.lecture?.name}과목의 {noti.content} 과제가 변경되었어요!
 							</p>
@@ -153,15 +186,15 @@ function Alarm() {
 							>
 								읽음
 							</StyleRefuseBtn>
-						</div>
+						</StyledLink>
 					))}
 			{notifications &&
 				notifications
 					.filter(notification => notification.notificationType === 'HS')
 					.map(noti => (
-						<div>
+						<StyledLink to={`/lecture/${noti.lecture?.id}`}>
 							<p>
-								{noti.lecture?.name}과목의 {noti.fromUser?.firstName}가 {noti.content}
+								{noti.lecture?.name} 과목의 {noti.fromUser?.firstName}가 {noti.content}
 								과제를 제출했어요!
 							</p>
 							<StyleRefuseBtn
@@ -178,8 +211,17 @@ function Alarm() {
 							>
 								읽음
 							</StyleRefuseBtn>
-						</div>
+						</StyledLink>
 					))}
+			{notifications.length === 0 && (
+				<StyledTitle>새로운 소식이 없어요~</StyledTitle>
+			)}
+
+			{notifications.length !== 0 && (
+				<footer>
+					<Pagination total={total} limit={limit} page={page} setPage={setPage} />
+				</footer>
+			)}
 		</>
 	);
 }
