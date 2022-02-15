47f3e3c454802b0cff6eb8b28aaf15a0024302df
이영준 <jun2korea@naver.com>
2022-02-15 19:52:18 +0900

    Refactor: Schools admin, models, serializers, urls
    Former-commit-id: 527dd7d6fb22a6459f1714f7fed985415af08368

--- backend/schools/serializers.py
+++ backend/schools/serializers.py
@@ -1,42 +1,52 @@
+"""Serializers for schools app
+"""
 from rest_framework import serializers
 
+from accounts.models import Student, Teacher, User
+from accounts.serializers.user import UserBasicSerializer
 from . models import (
     School, Lecture, Classroom,
     Homework, HomeworkSubmission, HomeworkSubmissionFiles,
     Article
 )
-from accounts.models import Student, Teacher, User
-from accounts.serializers.user import UserBasicSerializer
 
 class StudentBasciSerializer(serializers.ModelSerializer):
+    """Student Basic Srializer
+    """
     user = UserBasicSerializer()
-    
+
     class Meta:
         model = Student
         fields = ('user',)
 
 
 class TeacherBasicSerialzier(serializers.ModelSerializer):
+    """Teacher Basic Serializer
+    """
     user = UserBasicSerializer()
-    
+
     class Meta:
         model = Teacher
         fields = ('user',)
 
 
 class SchoolSerializer(serializers.ModelSerializer):
-    
+    """School Serializer
+    """
+
     class Meta:
         model = School
         fields = '__all__'
 
 
 class LectureSerializer(serializers.ModelSerializer):
-    
+    """Lecture Serializer
+    """
+
     class Meta:
         model = Lecture
         fields = '__all__'
-    
+
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['teacher'] = UserBasicSerializer(
@@ -45,39 +55,49 @@ class LectureSerializer(serializers.ModelSerializer):
 
 
 class LectrueDetailSerializer(serializers.ModelSerializer):
+    """Lecture Detail Serializer
+    """
     student_list = StudentBasciSerializer(many=True)
     teacher = TeacherBasicSerialzier()
-    
+
     class Meta:
         model = Lecture
         fields = '__all__'
 
 
 class ClassroomSerializer(serializers.ModelSerializer):
-    
+    """Classroom Serializer
+    """
+
     class Meta:
         model = Classroom
         fields = '__all__'
 
 
 class ClassroomDetailSerializer(serializers.ModelSerializer):
+    """ClassroomDetail Serializer
+    """
     school = SchoolSerializer(read_only=True)
     student_list = StudentBasciSerializer(many=True, read_only=True)
     teacher = TeacherBasicSerialzier(read_only=True)
-    
+
     class Meta:
         model = Classroom
         fields = '__all__'
 
 
 class HomeworkSerializer(serializers.ModelSerializer):
-    
+    """Homework Serializer
+    """
+
     class Meta:
         model = Homework
         fields = '__all__'
 
 
 class HomeworkSubmissionSerialzier(serializers.ModelSerializer):
+    """Homework Submission Serializer
+    """
 
     def __init__(self, *args, **kwargs):
         file_fields = kwargs.pop('file_fields', None)
@@ -90,6 +110,8 @@ class HomeworkSubmissionSerialzier(serializers.ModelSerializer):
             self.fields.update(**field_update_dict)
 
     class HomeworkSubmissionFilesSerializer(serializers.ModelSerializer):
+        """Homework Submission Files Serializer for HomeworkSubmissionSerializer
+        """
         class Meta:
             model = HomeworkSubmissionFiles
             fields = '__all__'
@@ -113,24 +135,29 @@ class HomeworkSubmissionSerialzier(serializers.ModelSerializer):
 
 
 class HomeworkDetailSerializer(serializers.ModelSerializer):
+    """Homework Detail Serializer
+    """
     submission = HomeworkSubmissionSerialzier(many=True)
-    
+
     class Meta:
         model = Homework
         fields = '__all__'
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    
-    
+    """Article Serializer
+    """
+
     class Meta:
         model = Article
         fields = '__all__'
 
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
+    """Article Detail Serializer
+    """
     writer = UserBasicSerializer()
-    
+
     class Meta:
         model = Article
         fields = '__all__'
